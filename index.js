8e45cec9b3376d73a262d725d8bb7aef0b6fb8be
Andres Glasener <52112240+Aglasener@users.noreply.github.com>
2020-01-22 17:53:31 -0500

    completed the code to create a resume html

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function accessGithub (answers) {
 
 }
 
-function generateHTML(info) {
+function generateHTML(info, colorChoice) {
     return `
 <!DOCTYPE html>
 <html lang="en">
@@ -38,16 +38,37 @@ function generateHTML(info) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
+  <style>
+  .container{background-color:${colorChoice}}
+  .jumbotron{background-color:${colorChoice}}  
+  </style>
+  <title>My Resume</title>
 </head>
 <body>
   <div class="jumbotron jumbotron-fluid">
   <div class="container">
-    <h1 class="display-4">Hi! My name is ${info.name}</h1>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+    <h1 class="display-4 justify-content-center" style = "background-color: white;">
+      <div class="row">
+          <div class="col-md">
+            <img src="${info.avatar_url}"></img>
+          </div>
+          <div class="col-md justify-content-center align-self-center">
+          ${info.name}
+          </br>  
+          (${info.login})
+          </div>
+      </div>
+    </h1>
+    
     <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${info.avatar_url}</li>
-      <li class="list-group-item">LinkedIn: ${info.public_repos}</li>
+      <li class="list-group-item"><a href ="${info.html_url}">My GitHub Profile</a></li>
+      <li class="list-group-item"><a href ="${info.blog}">My Blog</a></li>
+      <li class="list-group-item"><a href="https://www.google.com/maps/place/${info.location}">My Location</a></li>
+      <li class="list-group-item">User Bio: ${info.bio}</li>
+      <li class="list-group-item">Number of Public Repositories: ${info.public_repos}</li>
+      <li class="list-group-item">Number of Followers: ${info.followers}</li>
+      <li class="list-group-item">Number of GitHub stars: ${info.public_gists}</li>
+      <li class="list-group-item">Number of Users Following: ${info.following}</li>
     </ul>
   </div>
 </div>
@@ -60,7 +81,7 @@ async function init() {
         const answers = await promptUser();
         const response = await accessGithub(answers);
         console.log(response.data);
-        const html = await generateHTML(response.data);
+        const html = await generateHTML(response.data,answers.color);
         await writeFileAsync("index.html", html);
         console.log("successfully wrote to index.html");
     } catch(err) {
